22335ddb28ddf8c07db04524d9b8aa923774a34f
ocurieles-INGEINT <none@none>
2018-07-05 17:59:41 -0500

    Create INGEINT Resources

---
+++ src/com/ingeint/component/ProcessFactory.java
@@ -0,0 +1,18 @@
+package com.ingeint.component;
+
+import com.ingeint.base.CustomProcessFactory;
+
+public class ProcessFactory extends CustomProcessFactory{
+	/**
+	 * For initialize class. Register the process to build
+	 * 
+	 * <pre>
+	 * protected void initialize() {
+	 * 	registerProcess(PPrintPluginInfo.class);
+	 * }
+	 * </pre>
+	 */
+	@Override
+	protected void initialize() {
+	}
+}
