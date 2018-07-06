aa44d17d989d292914c9a7af89109773b2d2261d
ocurieles-INGEINT <none@none>
2018-07-05 19:18:20 -0500

    Personal requisition

---
+++ src/com/ingeint/process/HTLoadProspect.java
@@ -0,0 +1,19 @@
+package com.ingeint.process;
+
+import com.ingeint.base.CustomProcess;
+
+public class HTLoadProspect extends CustomProcess{
+
+	@Override
+	protected void prepare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
